d2f5c7477a141843abff039bec79382a61b7a4df
Lancenas <32763979+Lancenas@users.noreply.github.com>
2022-12-24 14:38:22 +0800

    Update and rename music.htm to index.php

--- index.php
+++ index.php
@@ -135,58 +135,33 @@
 			<textarea id="log"></textarea>
 		</div>
 	</div>
-	<p style="color:white; font-size:10px; text-align: center">	
-		<script language=JavaScript>
-			<!--
-				var caution = false
-				function setCookie(name, value, expires, path, domain, secure) {
-					var curCookie = name + "=" + escape(value) +
-					((expires) ? "; expires=" + expires.toGMTString() : "") +
-					((path) ? "; path=" + path : "") +
-					((domain) ? "; domain=" + domain : "") +
-					((secure) ? "; secure" : "")
-					if (!caution || (name + "=" + escape(value)).length <= 4000)
-					document.cookie = curCookie
-					else
-					if (confirm("Cookie exceeds 4KB and will be cut!"))
-					document.cookie = curCookie
-					}
-				function getCookie(name) {
-					var prefix = name + "="
-					var cookieStartIndex = document.cookie.indexOf(prefix)
-					if (cookieStartIndex == -1)
-					return null
-					var cookieEndIndex = document.cookie.indexOf(";", cookieStartIndex + prefix.length)
-					if (cookieEndIndex == -1)
-					cookieEndIndex = document.cookie.length
-					return unescape(document.cookie.substring(cookieStartIndex + prefix.length, cookieEndIndex))
-					}
-				function deleteCookie(name, path, domain) {
-					if (getCookie(name)) {
-					document.cookie = name + "=" +
-					((path) ? "; path=" + path : "") +
-					((domain) ? "; domain=" + domain : "") +
-					"; expires=Thu, 01-Jan-70 00:00:01 GMT"
-					}
-					}
-				function fixDate(date) {
-					var base = new Date(0)
-					var skew = base.getTime()
-					if (skew > 0)
-					date.setTime(date.getTime() - skew)
-				}
-				var now = new Date()
-				fixDate(now)
-				now.setTime(now.getTime() + 365 * 24 * 60 * 60 * 1000)
-				var visits = getCookie("counter")
-				if (!visits)
-				visits = 1
-				else
-				visits = parseInt(visits) + 1
-				setCookie("counter", visits, now)
-				document.write("您访问了本站" + visits + "次！")
-			// -->
-		</script>
-	</p>
+	<?php
+   		$max_len = 9;
+		$CounterFile = "counter.dat";
+		if(!file_exists($CounterFile)){  
+			 $counter = 0;     
+			 $cf = fopen($CounterFile,"w");  
+			 fputs($cf,'0'); 
+			 fclose($cf);  
+        }
+		else{  
+		    $cf = fopen($CounterFile,"r");
+		    $counter = trim(fgets($cf,$max_len));
+		    fclose($cf);
+		}
+		$counter++;  
+		$cf = fopen($CounterFile,"w");  
+		fputs($cf,$counter);
+		fclose($cf);
+	?>
+	
+	<div id="dd" align=center>
+	  <span>欢迎您!</span>
+	  <span>您是本站的第
+	   <?php 
+	   echo $counter; 
+	   ?>
+	  位访客！</span>
+	</div>
 </body>
 </html>
