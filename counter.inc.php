b4e8175a457af11e182f80064dca00444532bd0f
Lancenas <32763979+Lancenas@users.noreply.github.com>
2022-12-25 21:56:30 +0800

    Create counter.inc.php
    Page browsing statistics

---
+++ counter.inc.php
@@ -0,0 +1,19 @@
+<?php
+   	$max_len = 9;
+		$CounterFile = "./counter.dat";
+		if(!file_exists($CounterFile)){
+			 $counter = 0;     
+			 $cf = fopen($CounterFile,"w");
+			 fputs($cf,'0');
+			 fclose($cf); 
+        }
+		else{ 
+		    $cf = fopen($CounterFile,"r");
+		    $counter = trim(fgets($cf,$max_len));
+		    fclose($cf);
+		}
+		$counter++;
+		$cf = fopen($CounterFile,"w");
+		fputs($cf,$counter);
+		fclose($cf);
+	?>
